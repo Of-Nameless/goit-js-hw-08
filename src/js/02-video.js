624dff7fafa3cd950832f5ab202abfa44dfa8a00
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-01-30 20:48:22 +0100

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,15 +9,15 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(e) {
     const currentTime = e.seconds;
-    console.log(e.seconds);
+    // console.log(e.seconds);
     localStorage.setItem(LOCALESTORAGE_KEY, JSON.stringify(currentTime))
 }
 
 const currentTime = localStorage.getItem(LOCALESTORAGE_KEY);
 
-player.setCurrentTime(currentTime).then(function (seconds) {
-
-}).catch(function(error) {
+player.setCurrentTime(currentTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
      switch (error.name) {
        case 'RangeError':
             break;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,27 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const LOCALESTORAGE_KEY = "feedback-form-state"
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input')
+const LOCALESTORAGE_KEY = "feedback-form-state";
 
 form.addEventListener('input', throttle((e) => {
     let persistedString = localStorage.getItem(LOCALESTORAGE_KEY);
     // console.log(persistedString);
     persistedString = persistedString ? JSON.parse(persistedString) : {};
     persistedString[e.target.name] = e.target.value;
+    // if (persistedString) {
+    //     textarea.value = persistedString;
+    //     input.value = persistedString;
+    // }
+    // console.log(e.target.value);
+    // console.log(e.target.name);
     localStorage.setItem(LOCALESTORAGE_KEY, JSON.stringify(persistedString));
 }, 500));
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    e.target.reset();
     const formData = new FormData(form);
     formData.forEach((email, message) => console.log(email, message));
     localStorage.removeItem(LOCALESTORAGE_KEY);
