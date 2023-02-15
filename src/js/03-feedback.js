9ee9e192d3fc9a7e4e840d837b59980aba271e19
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-02-15 19:52:22 +0100

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,44 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea');
-const input = document.querySelector('.feedback-form input')
-const LOCALESTORAGE_KEY = "feedback-form-state";
+// console.log(form);
 
-form.addEventListener('input', throttle((e) => {
-    let persistedString = localStorage.getItem(LOCALESTORAGE_KEY);
-    // console.log(persistedString);
-    persistedString = persistedString ? JSON.parse(persistedString) : {};
+const LOCALESTORAGE_KEY = 'feedback-form-state';
+let persistedString = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+reloadForm();
+
+function onFormInput(e) {
     persistedString[e.target.name] = e.target.value;
-    // if (persistedString) {
-    //     textarea.value = persistedString;
-    //     input.value = persistedString;
-    // }
-    // console.log(e.target.value);
-    // console.log(e.target.name);
-    localStorage.setItem(LOCALESTORAGE_KEY, JSON.stringify(persistedString));
-}, 500));
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    e.target.reset();
-    const formData = new FormData(form);
-    formData.forEach((email, message) => console.log(email, message));
+    localStorage.setItem(LOCALESTORAGE_KEY, JSON.stringify(persistedString));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const {email, message} = e.target;
+    if (email.value === '' || message.value === '') {
+      return;
+  }
+
+    form.reset();
+    // console.log(persistedString);
+
     localStorage.removeItem(LOCALESTORAGE_KEY);
-});
+    persistedString = {};
+}
+
+function reloadForm() {
+    const getStorage = localStorage.getItem(LOCALESTORAGE_KEY);
+
+    if (getStorage) {
+        persistedString = JSON.parse(getStorage);
+    }
+    for (const key in persistedString) {
+        form.elements[key].value = persistedString[key];
+    }
+}
